4396050339d6376af0efe7a197e6aa6f3a20ea7d
Will Strinz <will.strinz@drip.com>
2021-12-06 14:31:36 -0600

    golf

--- lib/aoc2021/day6.ex
+++ lib/aoc2021/day6.ex
@@ -4,21 +4,12 @@ defmodule Aoc2021.Day6 do
     |> Enum.reduce(input, fn num, counts ->
       Map.put(counts, num - 1, Map.get(input, num, 0))
     end)
-    |> Map.update(6, 0, fn last_sixes ->
-      last_sixes + Map.get(input, 0, 0)
-    end)
+    |> Map.update(6, 0, &(&1 + Map.get(input, 0, 0)))
     |> Map.put(8, Map.get(input, 0, 0))
   end
 
-  def part1(input) do
-    0..79
-    |> Enum.reduce(input, fn _, state -> tick(state) end)
-    |> Map.values()
-    |> Enum.sum()
-  end
-
-  def part2(input) do
-    0..255
+  def fish_at_tick(input, ticks) do
+    0..(ticks - 1)
     |> Enum.reduce(input, fn _, state -> tick(state) end)
     |> Map.values()
     |> Enum.sum()
@@ -32,6 +23,6 @@ defmodule Aoc2021.Day6 do
       |> Enum.map(&elem(&1, 0))
       |> Enum.frequencies()
 
-    [part1(input), part2(input)]
+    [fish_at_tick(input, 80), fish_at_tick(input, 256)]
   end
 end
